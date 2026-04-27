7690e1759d194133c0d56d818209a61d62afd5f3
Pedro Faleiros <pedro.alves.faleiros@gmail.com>
2026-04-27 19:53:41 -0300

    feat: add validator for email and password validation in CreateUserController

--- src/controllers/create-user.js
+++ src/controllers/create-user.js
@@ -1,4 +1,6 @@
 import { CreateUserUseCase } from '../use-cases/create-user.js';
+import validator from 'validator';
+import { badRequest, created, serverError } from './helpers.js';
 
 export class CreateUserController {
   async execute(httpRequest) {
@@ -9,32 +11,32 @@ export class CreateUserController {
 
       for (const field of requiredFields) {
         if (!params[field] || params[field].trim().length === 0) {
-          return {
-            statusCode: 400,
-            body: {
-              errorMessage: `Missing param: ${field}`,
-            },
-          };
+          return badRequest({ message: `Missing param: ${field}` });
         }
+      }
+
+      const emailIsValid = validator.isEmail(params.email);
+
+      if (!emailIsValid) {
+        return badRequest({ message: 'Invalid e-mail format' });
+      }
+
+      const passwordIsValid = params.password.length >= 6;
+
+      if (!passwordIsValid) {
+        return badRequest({
+          message: 'Password must be at least 6 characters long',
+        });
       }
 
       const createUserUseCase = new CreateUserUseCase();
 
       const createdUser = await createUserUseCase.execute(params);
 
-      return {
-        statusCode: 201,
-        body: createdUser,
-      };
+      return created(createdUser);
     } catch (error) {
       console.error('Error creating user:', error);
-
-      return {
-        statusCode: 500,
-        body: {
-          errorMessage: 'Internal server error',
-        },
-      };
+      return serverError();
     }
   }
 }
