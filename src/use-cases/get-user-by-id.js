5fbe8eff88e86de94f1f4ba62d7c45c0459af63e
Pedro Faleiros <pedro.alves.faleiros@gmail.com>
2026-05-05 20:48:06 -0300

    feat: add DeleteUserUseCase

--- src/use-cases/get-user-by-id.js
+++ src/use-cases/get-user-by-id.js
@@ -2,9 +2,9 @@ import { PostgresGetUserByIdRepository } from '../repositories/postgres/index.js
 
 export class GetUserByIdUseCase {
   async execute(userId) {
-    const getUserByIdRepository = new PostgresGetUserByIdRepository();
+    const postgresGetUserByIdRepository = new PostgresGetUserByIdRepository();
 
-    const user = await getUserByIdRepository.execute(userId);
+    const user = await postgresGetUserByIdRepository.execute(userId);
 
     return user;
   }
