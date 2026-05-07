e3b068849df1d199748e958777959c1bb7af3e0e
Pedro Faleiros <pedro.alves.faleiros@gmail.com>
2026-05-06 21:10:38 -0300

    refactor: implement dependency injection in CreateUserController and CreateUserUseCase

--- src/use-cases/create-user.js
+++ src/use-cases/create-user.js
@@ -1,18 +1,17 @@
 import { v4 as uuidv4 } from 'uuid';
 import bcrypt from 'bcrypt';
-import {
-  PostgresCreateUserRepository,
-  PostgresGetUserByEmailRepository,
-} from '../repositories/postgres/index.js';
 import { EmailAlreadyInUseError } from '../errors/user.js';
 
 export class CreateUserUseCase {
-  async execute(createUserParams) {
-    const postgresGetUserByEmailRepository =
-      new PostgresGetUserByEmailRepository();
+  constructor(getUserByEmailRepository, createUserRepository) {
+    this.getUserByEmailRepository = getUserByEmailRepository;
+    this.createUserRepository = createUserRepository;
+  }
 
-    const userWithProvidedEmail =
-      await postgresGetUserByEmailRepository.execute(createUserParams.email);
+  async execute(createUserParams) {
+    const userWithProvidedEmail = await this.getUserByEmailRepository.execute(
+      createUserParams.email,
+    );
 
     if (userWithProvidedEmail) {
       throw new EmailAlreadyInUseError(createUserParams.email);
@@ -28,9 +27,7 @@ export class CreateUserUseCase {
       password: hashedPassword,
     };
 
-    const postgresCreateUserRepository = new PostgresCreateUserRepository();
-
-    const createdUser = await postgresCreateUserRepository.execute(user);
+    const createdUser = await this.createUserRepository.execute(user);
 
     return createdUser;
   }
