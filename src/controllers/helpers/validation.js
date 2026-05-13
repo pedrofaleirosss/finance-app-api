1b04159aac9e4c8e247c3e8c373abe1cd95a057c
Pedro Faleiros <pedro.alves.faleiros@gmail.com>
2026-05-12 21:10:31 -0300

    refactor: add validate required fields helper function

--- src/controllers/helpers/validation.js
+++ src/controllers/helpers/validation.js
@@ -5,3 +5,23 @@ export const checkIfIdIsValid = (id) => validator.isUUID(id);
 
 export const invalidIdResponse = () =>
   badRequest({ message: 'The provided ID is not valid.' });
+
+export const checkIfIsString = (value) => typeof value === 'string';
+
+export const validateRequiredFields = (params, requiredFields) => {
+  for (const field of requiredFields) {
+    const fieldIsMissing = !params[field];
+    const fieldIsEmpty =
+      checkIfIsString(params[field]) &&
+      validator.isEmpty(params[field], { ignore_whitespace: true });
+
+    if (fieldIsMissing || fieldIsEmpty) {
+      return {
+        missingField: field,
+        ok: false,
+      };
+    }
+  }
+
+  return { ok: true, missingField: null };
+};
