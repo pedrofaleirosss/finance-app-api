711730326e6f06b7971dd6a4573a7e48eacd9bb9
Pedro Faleiros <pedro.alves.faleiros@gmail.com>
2026-05-16 19:55:09 -0300

    feat: add UpdateTransactionController

---
+++ src/controllers/transaction/update-transaction.js
@@ -0,0 +1,73 @@
+import {
+  badRequest,
+  checkIfAmountIsValid,
+  checkIfIdIsValid,
+  checkIfTypeIsValid,
+  invalidAmountResponse,
+  invalidIdResponse,
+  invalidTypeResponse,
+  ok,
+  serverError,
+} from '../helpers/index.js';
+
+export class UpdateTransactionController {
+  constructor(updateTransactionUseCase) {
+    this.updateTransactionUseCase = updateTransactionUseCase;
+  }
+
+  async execute(httpRequest) {
+    try {
+      const idIsValid = checkIfIdIsValid(httpRequest.params.transactionId);
+
+      if (!idIsValid) {
+        return invalidIdResponse();
+      }
+
+      const params = httpRequest.body;
+
+      const allowedFields = ['name', 'date', 'amount', 'type'];
+
+      const someFieldIsNotAllowed = Object.keys(params).some(
+        (field) => !allowedFields.includes(field),
+      );
+
+      if (someFieldIsNotAllowed) {
+        return badRequest({ message: 'Some provided field is not allowed.' });
+      }
+
+      for (const field of Object.keys(params)) {
+        if (params[field].trim().length === 0) {
+          return badRequest({
+            message: `The field ${field} is blank. Please provide a value.`,
+          });
+        }
+      }
+
+      if (params.amount) {
+        const amountIsValid = checkIfAmountIsValid(params.amount);
+
+        if (!amountIsValid) {
+          return invalidAmountResponse();
+        }
+      }
+
+      if (params.type) {
+        const typeIsValid = checkIfTypeIsValid(params.type);
+
+        if (!typeIsValid) {
+          return invalidTypeResponse();
+        }
+      }
+
+      const transaction = await this.updateTransactionUseCase.execute(
+        httpRequest.params.transactionId,
+        params,
+      );
+
+      return ok(transaction);
+    } catch (error) {
+      console.log(error);
+      return serverError();
+    }
+  }
+}
