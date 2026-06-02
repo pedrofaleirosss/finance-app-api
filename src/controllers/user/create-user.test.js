66c2ba89f97a1a21beff23147f06c71bc3e39a7c
Pedro Faleiros <pedro.alves.faleiros@gmail.com>
2026-06-02 20:49:44 -0300

    test: ensure 400 is returned if password is not provided

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -94,4 +94,21 @@ describe('Create User Controller', () => {
 
     expect(result.statusCode).toBe(400);
   });
+
+  it('should return 400 if password is not provided', async () => {
+    const createUserUseCase = new CreateUserUseCaseStub();
+    const createUserController = new CreateUserController(createUserUseCase);
+
+    const httpRequest = {
+      body: {
+        first_name: 'Jane',
+        last_name: 'Doe',
+        email: 'jane@doe.com',
+      },
+    };
+
+    const result = await createUserController.execute(httpRequest);
+
+    expect(result.statusCode).toBe(400);
+  });
 });
