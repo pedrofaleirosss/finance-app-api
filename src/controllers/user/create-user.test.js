22b0fcec330218ee011921b19d8ada276cc9792c
Pedro Faleiros <pedro.alves.faleiros@gmail.com>
2026-06-02 20:41:55 -0300

    test: ensure 400 is returned if first_name is not provided

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -25,4 +25,21 @@ describe('Create User Controller', () => {
     expect(result.statusCode).toBe(201);
     expect(result.body).toBe(httpRequest.body);
   });
+
+  it('should return 400 if first_name is not provided', async () => {
+    const createUserUseCase = new CreateUserUseCaseStub();
+    const createUserController = new CreateUserController(createUserUseCase);
+
+    const httpRequest = {
+      body: {
+        last_name: 'Doe',
+        email: 'jane@doe.com',
+        password: '123456',
+      },
+    };
+
+    const result = await createUserController.execute(httpRequest);
+
+    expect(result.statusCode).toBe(400);
+  });
 });
