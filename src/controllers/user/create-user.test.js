9ae6f1ab9b7af550747766278faa0c0207ca8bb2
Pedro Faleiros <pedro.alves.faleiros@gmail.com>
2026-06-04 22:23:01 -0300

    test: ensure CreateUserController returns 500 if CreateUserUseCase throws

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -151,4 +151,26 @@ describe('Create User Controller', () => {
 
     expect(executeSpy).toHaveBeenCalledWith(httpRequest.body);
   });
+
+  it('should return 500 if CreateUserUseCase throws an error', async () => {
+    const createUserUseCase = new CreateUserUseCaseStub();
+    const createUserController = new CreateUserController(createUserUseCase);
+
+    const httpRequest = {
+      body: {
+        first_name: faker.person.firstName(),
+        last_name: faker.person.lastName(),
+        email: faker.internet.email(),
+        password: faker.internet.password({ length: 6 }),
+      },
+    };
+
+    jest.spyOn(createUserUseCase, 'execute').mockImplementationOnce(() => {
+      throw new Error('Test error');
+    });
+
+    const result = await createUserController.execute(httpRequest);
+
+    expect(result.statusCode).toBe(500);
+  });
 });
