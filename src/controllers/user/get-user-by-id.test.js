be235ae89354b83c8c92773893d171c4075dcb8b
Pedro Faleiros <pedro.alves.faleiros@gmail.com>
2026-06-09 23:08:31 -0300

    test: ensure GetUserByIdController returns 400 if an invalid userId is provided

--- src/controllers/user/get-user-by-id.test.js
+++ src/controllers/user/get-user-by-id.test.js
@@ -30,4 +30,12 @@ describe('Get User By Id Controller', () => {
 
     expect(result.statusCode).toBe(200);
   });
+
+  it('should return 400 if an invalid userId is provided', async () => {
+    const { sut } = makeSut();
+
+    const result = await sut.execute({ params: { userId: 'invalid-uuid' } });
+
+    expect(result.statusCode).toBe(400);
+  });
 });
