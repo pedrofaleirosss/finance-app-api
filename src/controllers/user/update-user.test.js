da68945783b5caed0d57d9d0e1469241eaa5cfdc
Pedro Faleiros <pedro.alves.faleiros@gmail.com>
2026-06-10 23:46:45 -0300

    test: ensure UpdateUserController returns 400 if an invalid email is provided

--- src/controllers/user/update-user.test.js
+++ src/controllers/user/update-user.test.js
@@ -34,4 +34,18 @@ describe('Update User Controller', () => {
 
     expect(result.statusCode).toBe(200);
   });
+
+  it('should return 400 if an invalid email is provided', async () => {
+    const { sut } = makeSut();
+
+    const result = await sut.execute({
+      params: httpRequest.params,
+      body: {
+        ...httpRequest.body,
+        email: 'invalid-email',
+      },
+    });
+
+    expect(result.statusCode).toBe(400);
+  });
 });
