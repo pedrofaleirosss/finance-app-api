d034b97ed2b38b46c9217052524aab4cca461526
Pedro Faleiros <pedro.alves.faleiros@gmail.com>
2026-06-11 22:24:55 -0300

    test: ensure CreateTransactionController returns 400 if amount is not a valid currency

--- src/controllers/transaction/create-transaction.test.js
+++ src/controllers/transaction/create-transaction.test.js
@@ -149,4 +149,17 @@ describe('Create Transaction Controller', () => {
 
     expect(response.statusCode).toBe(400);
   });
+
+  it('should return 400 if amount is not a valid currency', async () => {
+    const { sut } = makeSut();
+
+    const response = await sut.execute({
+      body: {
+        ...baseHttpRequest.body,
+        amount: 'invalid-amount',
+      },
+    });
+
+    expect(response.statusCode).toBe(400);
+  });
 });
