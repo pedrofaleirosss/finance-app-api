45028bb773e8dc3c994ed74a9471da2c80f45724
Pedro Faleiros <pedro.alves.faleiros@gmail.com>
2026-06-12 21:32:01 -0300

    test: ensure DeleteTransactionController returns 400 if transactionId is invalid

--- src/controllers/transaction/delete-transaction.test.js
+++ src/controllers/transaction/delete-transaction.test.js
@@ -31,4 +31,14 @@ describe('Delete Transaction Controller', () => {
 
     expect(response.statusCode).toBe(200);
   });
+
+  it('should return 400 if transactionId is invalid', async () => {
+    const { sut } = makeSut();
+
+    const response = await sut.execute({
+      params: { transactionId: 'invalid-uuid' },
+    });
+
+    expect(response.statusCode).toBe(400);
+  });
 });
