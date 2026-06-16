150721ab0719c2eb735fdd39e666fd5f1332e8c1
Pedro Faleiros <pedro.alves.faleiros@gmail.com>
2026-06-16 19:39:46 -0300

    test: ensure UpdateUserController calls UpdateUserUseCase with correct params

--- src/controllers/user/update-user.test.js
+++ src/controllers/user/update-user.test.js
@@ -113,4 +113,16 @@ describe('Update User Controller', () => {
 
     expect(result.statusCode).toBe(400);
   });
+
+  it('should call UpdateUserUseCase with correct params', async () => {
+    const { sut, updateUserUseCase } = makeSut();
+    const executeSpy = jest.spyOn(updateUserUseCase, 'execute');
+
+    await sut.execute(httpRequest);
+
+    expect(executeSpy).toHaveBeenCalledWith(
+      httpRequest.params.userId,
+      httpRequest.body,
+    );
+  });
 });
