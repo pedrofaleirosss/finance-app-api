8802ac0187f53659e40a8e082fbcb4808b5ffdfa
Pedro Faleiros <pedro.alves.faleiros@gmail.com>
2026-06-16 19:30:25 -0300

    test: ensure CreateTransactionController calls CreateTransactionUseCase with correct params

--- src/controllers/transaction/create-transaction.test.js
+++ src/controllers/transaction/create-transaction.test.js
@@ -174,4 +174,13 @@ describe('Create Transaction Controller', () => {
 
     expect(response.statusCode).toBe(500);
   });
+
+  it('should call CreateTransactionUseCase with correct params', async () => {
+    const { sut, createTransactionUseCase } = makeSut();
+    const executeSpy = jest.spyOn(createTransactionUseCase, 'execute');
+
+    await sut.execute(baseHttpRequest);
+
+    expect(executeSpy).toHaveBeenCalledWith(baseHttpRequest.body);
+  });
 });
