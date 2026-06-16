a54c69efe7a13c730396f097b59f5722b768ad68
Pedro Faleiros <pedro.alves.faleiros@gmail.com>
2026-06-16 19:34:50 -0300

    test: ensure GetUserBalanceController calls GetUserBalanceUseCase with correct params

--- src/controllers/user/get-user-balance.test.js
+++ src/controllers/user/get-user-balance.test.js
@@ -53,4 +53,13 @@ describe('Get User Balance Controller', () => {
 
     expect(result.statusCode).toBe(500);
   });
+
+  it('should call GetUserBalanceUseCase with correct params', async () => {
+    const { sut, getUserBalanceUseCase } = makeSut();
+    const executeSpy = jest.spyOn(getUserBalanceUseCase, 'execute');
+
+    await sut.execute(httpRequest);
+
+    expect(executeSpy).toHaveBeenCalledWith(httpRequest.params.userId);
+  });
 });
