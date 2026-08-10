8a6d9a401d6a24d4040aae2cb721447c2ca7fa35
Pedro Faleiros <pedro.alves.faleiros@gmail.com>
2026-08-10 20:29:10 -0300

    feat: ensure CreateUserUseCase uses PasswordHasherAdapter

--- src/factories/controllers/user.js
+++ src/factories/controllers/user.js
@@ -1,3 +1,4 @@
+import { PasswordHasherAdapter } from '../../adapters/index.js';
 import {
   CreateUserController,
   DeleteUserController,
@@ -34,10 +35,12 @@ export const makeGetUserByIdController = () => {
 export const makeCreateUserController = () => {
   const getUserByEmailRepository = new PostgresGetUserByEmailRepository();
   const createUserRepository = new PostgresCreateUserRepository();
+  const passwordHasherAdapter = new PasswordHasherAdapter();
 
   const createUserUseCase = new CreateUserUseCase(
     getUserByEmailRepository,
     createUserRepository,
+    passwordHasherAdapter,
   );
 
   const createUserController = new CreateUserController(createUserUseCase);
