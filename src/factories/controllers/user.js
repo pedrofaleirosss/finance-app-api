8bd098149fbc25f53dad93b0c29b023efe182166
Pedro Faleiros <pedro.alves.faleiros@gmail.com>
2026-08-10 20:34:09 -0300

    feat: ensure IdGeneratorAdapter is used on CreateUserUseCase

--- src/factories/controllers/user.js
+++ src/factories/controllers/user.js
@@ -1,4 +1,7 @@
-import { PasswordHasherAdapter } from '../../adapters/index.js';
+import {
+  IdGeneratorAdapter,
+  PasswordHasherAdapter,
+} from '../../adapters/index.js';
 import {
   CreateUserController,
   DeleteUserController,
@@ -36,11 +39,13 @@ export const makeCreateUserController = () => {
   const getUserByEmailRepository = new PostgresGetUserByEmailRepository();
   const createUserRepository = new PostgresCreateUserRepository();
   const passwordHasherAdapter = new PasswordHasherAdapter();
+  const idGeneratorAdapter = new IdGeneratorAdapter();
 
   const createUserUseCase = new CreateUserUseCase(
     getUserByEmailRepository,
     createUserRepository,
     passwordHasherAdapter,
+    idGeneratorAdapter,
   );
 
   const createUserController = new CreateUserController(createUserUseCase);
