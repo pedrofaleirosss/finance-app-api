54124db42300c1f861174392009d73b5b4378c64
Pedro Faleiros <pedro.alves.faleiros@gmail.com>
2026-08-11 20:33:57 -0300

    refactor: remove unused code

--- src/controllers/helpers/user.js
+++ src/controllers/helpers/user.js
@@ -1,12 +1,4 @@
-import { badRequest, notFound } from './http.js';
-
-export const invalidPasswordResponse = () =>
-  badRequest({
-    message: 'Password must be at least 6 characters long.',
-  });
-
-export const invalidEmailResponse = () =>
-  badRequest({ message: 'Invalid e-mail format.' });
+import { notFound } from './http.js';
 
 export const userNotFoundResponse = () =>
   notFound({ message: 'User not found.' });
