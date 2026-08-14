1195baeee79002165c2850e6de8c2dcc60833ff1
Pedro Faleiros <pedro.alves.faleiros@gmail.com>
2026-08-13 22:29:00 -0300

    test: ensure CreateUserUseCase throws if IdGeneratorAdapter throws

--- src/use-cases/user/create-user.test.js
+++ src/use-cases/user/create-user.test.js
@@ -118,4 +118,15 @@ describe('Create User Use Case', () => {
 
     await expect(promise).rejects.toThrow();
   });
+
+  it('should throw if IdGeneratorAdapter throws', async () => {
+    const { sut, idGeneratorAdapter } = makeSut();
+    jest
+      .spyOn(idGeneratorAdapter, 'execute')
+      .mockRejectedValueOnce(new Error());
+
+    const promise = sut.execute(user);
+
+    await expect(promise).rejects.toThrow();
+  });
 });
