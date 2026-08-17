1c0663e43111c54438ca42ad37e9e8d019a928da
Pedro Faleiros <pedro.alves.faleiros@gmail.com>
2026-08-17 20:25:58 -0300

    test: ensure GetUserByIdUseCase throws if GetUserByIdRepository throws

--- src/use-cases/user/get-user-by-id.test.js
+++ src/use-cases/user/get-user-by-id.test.js
@@ -41,4 +41,15 @@ describe('Get User By Id Use Case', () => {
 
     expect(executeSpy).toHaveBeenCalledWith(userId);
   });
+
+  it('should throw if GetUserByIdRepository throws', async () => {
+    const { sut, getUserByIdRepository } = makeSut();
+    jest
+      .spyOn(getUserByIdRepository, 'execute')
+      .mockRejectedValueOnce(new Error());
+
+    const promise = sut.execute(faker.string.uuid());
+
+    await expect(promise).rejects.toThrow();
+  });
 });
