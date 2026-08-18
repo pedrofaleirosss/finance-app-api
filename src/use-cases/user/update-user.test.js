ebb0480407e498cdec3db2ad7a7904c31c1136ce
Pedro Faleiros <pedro.alves.faleiros@gmail.com>
2026-08-18 20:41:20 -0300

    test: ensure UpdateUserUseCase updates user successfully (with password)

--- src/use-cases/user/update-user.test.js
+++ src/use-cases/user/update-user.test.js
@@ -72,4 +72,18 @@ describe('Update User Use Case', () => {
     expect(getUserByEmailRepositorySpy).toHaveBeenCalledWith(email);
     expect(result).toBe(user);
   });
+
+  it('should update user successfully (with password)', async () => {
+    const { sut, passwordHasherAdapter } = makeSut();
+    const passwordHasherAdapterSpy = jest.spyOn(
+      passwordHasherAdapter,
+      'execute',
+    );
+    const password = faker.internet.password();
+
+    const result = await sut.execute(faker.string.uuid(), { password });
+
+    expect(passwordHasherAdapterSpy).toHaveBeenCalledWith(password);
+    expect(result).toBe(user);
+  });
 });
