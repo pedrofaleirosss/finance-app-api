6eb8b866aa002fa5cfdacef9b971d927aa195346
Pedro Faleiros <pedro.alves.faleiros@gmail.com>
2026-08-19 19:08:25 -0300

    test: ensure UpdateUserUseCase calls UpdateUserRepository with correct params

--- src/use-cases/user/update-user.test.js
+++ src/use-cases/user/update-user.test.js
@@ -100,4 +100,23 @@ describe('Update User Use Case', () => {
       new EmailAlreadyInUseError(user.email),
     );
   });
+
+  it('should call UpdateUserRepository with correct params', async () => {
+    const { sut, updateUserRepository } = makeSut();
+    const updateUserRepositorySpy = jest.spyOn(updateUserRepository, 'execute');
+
+    const updateUserParams = {
+      first_name: user.first_name,
+      last_name: user.last_name,
+      email: user.email,
+      password: user.password,
+    };
+
+    await sut.execute(user.id, updateUserParams);
+
+    expect(updateUserRepositorySpy).toHaveBeenCalledWith(user.id, {
+      ...updateUserParams,
+      password: 'hashed_password',
+    });
+  });
 });
