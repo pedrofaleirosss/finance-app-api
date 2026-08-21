b1d1cd9603bc5520dade806d12427a168a5770e1
Pedro Faleiros <pedro.alves.faleiros@gmail.com>
2026-08-21 20:56:32 -0300

    test: ensure CreateTransactionUseCase throws if IdGeneratorAdapter throws

--- src/use-cases/transaction/create-transaction.test.js
+++ src/use-cases/transaction/create-transaction.test.js
@@ -124,4 +124,15 @@ describe('Create Transaction Use Case', () => {
 
     await expect(promise).rejects.toThrow();
   });
+
+  it('should throw if IdGeneratorAdapter throws', async () => {
+    const { sut, idGeneratorAdapter } = makeSut();
+    jest
+      .spyOn(idGeneratorAdapter, 'execute')
+      .mockRejectedValueOnce(new Error());
+
+    const promise = sut.execute(createTransactionParams);
+
+    await expect(promise).rejects.toThrow();
+  });
 });
