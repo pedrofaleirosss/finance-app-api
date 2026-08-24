145277f939be19b9dd755fdf0513b8e25094e59e
Pedro Faleiros <pedro.alves.faleiros@gmail.com>
2026-08-24 19:12:00 -0300

    test: ensure UpdateTransactionUseCase throws if UpdateTransactionRepository throws

--- src/use-cases/transaction/update-transaction.test.js
+++ src/use-cases/transaction/update-transaction.test.js
@@ -54,4 +54,17 @@ describe('Update Transaction Use Case', () => {
       { amount: transaction.amount },
     );
   });
+
+  it('should throw if UpdateTransactionRepository throws', async () => {
+    const { sut, updateTransactionRepository } = makeSut();
+    jest
+      .spyOn(updateTransactionRepository, 'execute')
+      .mockRejectedValueOnce(new Error());
+
+    const promise = sut.execute(transaction.id, {
+      amount: transaction.amount,
+    });
+
+    await expect(promise).rejects.toThrow();
+  });
 });
