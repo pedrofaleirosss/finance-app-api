433484b33f010472abcb368c8c0c846bf44a7ce6
Pedro Faleiros <pedro.alves.faleiros@gmail.com>
2026-08-29 19:36:46 -0300

    test: ensure PostgresDeleteUserRepository calls Prisma with correct params

--- src/repositories/postgres/user/delete-user.test.js
+++ src/repositories/postgres/user/delete-user.test.js
@@ -1,6 +1,7 @@
 import { prisma } from '../../../../prisma/prisma.js';
 import { user } from '../../../tests/index.js';
 import { PostgresDeleteUserRepository } from './delete-user.js';
+import { jest } from '@jest/globals';
 
 describe('Postgres Delete User Repository', () => {
   it('should delete a user on db successfully', async () => {
@@ -13,5 +14,14 @@ describe('Postgres Delete User Repository', () => {
     const result = await sut.execute(user.id);
 
     expect(result).toStrictEqual(user);
+  });
+
+  it('should call Prisma with correct params', async () => {
+    const sut = new PostgresDeleteUserRepository();
+    const prismaSpy = jest.spyOn(prisma.user, 'delete');
+
+    await sut.execute(user.id);
+
+    expect(prismaSpy).toHaveBeenCalledWith({ where: { id: user.id } });
   });
 });
