37e73d4152b1c4c709df53fe8db82ce540185443
Pedro Faleiros <pedro.alves.faleiros@gmail.com>
2026-08-31 19:45:16 -0300

    test: ensure PostgresGetUserByIdRepository calls Prisma with correct params

--- src/repositories/postgres/user/get-user-by-id.test.js
+++ src/repositories/postgres/user/get-user-by-id.test.js
@@ -1,6 +1,7 @@
 import { prisma } from '../../../../prisma/prisma.js';
 import { user as fakeUser } from '../../../tests/index.js';
 import { PostgresGetUserByIdRepository } from './get-user-by-id.js';
+import { jest } from '@jest/globals';
 
 describe('Postgres Get User By Id Repository', () => {
   it('should get user by id on db successfully', async () => {
@@ -11,5 +12,18 @@ describe('Postgres Get User By Id Repository', () => {
     const result = await sut.execute(user.id);
 
     expect(result).toStrictEqual(user);
+  });
+
+  it('should call Prisma with correct params', async () => {
+    const sut = new PostgresGetUserByIdRepository();
+    const prismaSpy = jest.spyOn(prisma.user, 'findUnique');
+
+    await sut.execute(fakeUser.id);
+
+    expect(prismaSpy).toHaveBeenCalledWith({
+      where: {
+        id: fakeUser.id,
+      },
+    });
   });
 });
