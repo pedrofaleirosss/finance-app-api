f474749f4d371b4b44745ed50dc6df0919042ff4
Pedro Faleiros <pedro.alves.faleiros@gmail.com>
2026-09-01 20:12:09 -0300

    test: ensure PostgresCreateUserRepository throws if Prisma throws

--- src/repositories/postgres/user/create-user.test.js
+++ src/repositories/postgres/user/create-user.test.js
@@ -24,4 +24,13 @@ describe('Postgres Create User Repository', () => {
 
     expect(prismaSpy).toHaveBeenCalledWith({ data: user });
   });
+
+  it('should throw if Prisma throws', async () => {
+    const sut = new PostgresCreateUserRepository();
+    jest.spyOn(prisma.user, 'create').mockRejectedValueOnce(new Error());
+
+    const promise = sut.execute(user);
+
+    await expect(promise).rejects.toThrow();
+  });
 });
