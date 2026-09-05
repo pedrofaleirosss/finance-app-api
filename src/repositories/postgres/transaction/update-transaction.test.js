839dd5464f7f2dc948bfb77c87435881000edd88
Pedro Faleiros <pedro.alves.faleiros@gmail.com>
2026-09-04 21:26:10 -0300

    test: ensure PostgresUpdateTransactionRepository throws if Prisma throws

--- src/repositories/postgres/transaction/update-transaction.test.js
+++ src/repositories/postgres/transaction/update-transaction.test.js
@@ -51,4 +51,13 @@ describe('Postgres Update Transaction Repository', () => {
       data: { ...transaction, user_id: user.id },
     });
   });
+
+  it('should throw if Prisma throws', async () => {
+    const sut = new PostgresUpdateTransactionRepository();
+    jest.spyOn(prisma.transaction, 'update').mockRejectedValueOnce(new Error());
+
+    const promise = sut.execute(transaction.id, transaction);
+
+    await expect(promise).rejects.toThrow();
+  });
 });
