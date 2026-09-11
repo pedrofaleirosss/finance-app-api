e4046551f14ea0b255019ef3fb21128cfe94bf32
Pedro Faleiros <pedro.alves.faleiros@gmail.com>
2026-09-10 21:50:51 -0300

    refactor: move user routes to separate file

--- index.js
+++ index.js
@@ -1,62 +1,18 @@
 import 'dotenv/config.js';
 import express from 'express';
 import {
-  makeCreateUserController,
-  makeDeleteUserController,
-  makeGetUserBalanceController,
-  makeGetUserByIdController,
-  makeUpdateUserController,
-} from './src/factories/controllers/user.js';
-import {
   makeCreateTransactionController,
   makeDeleteTransactionController,
   makeGetTransactionsByUserIdController,
   makeUpdateTransactionController,
 } from './src/factories/controllers/transaction.js';
+import { usersRouter } from './src/routes/users.js';
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/api/users/:userId', async (request, response) => {
-  const getUserByIdController = makeGetUserByIdController();
-
-  const { statusCode, body } = await getUserByIdController.execute(request);
-
-  return response.status(statusCode).send(body);
-});
-
-app.get('/api/users/:userId/balance', async (request, response) => {
-  const getUserBalanceController = makeGetUserBalanceController();
-
-  const { statusCode, body } = await getUserBalanceController.execute(request);
-
-  return response.status(statusCode).send(body);
-});
-
-app.post('/api/users', async (request, response) => {
-  const createUserController = makeCreateUserController();
-
-  const { statusCode, body } = await createUserController.execute(request);
-
-  return response.status(statusCode).send(body);
-});
-
-app.patch('/api/users/:userId', async (request, response) => {
-  const updateUserController = makeUpdateUserController();
-
-  const { statusCode, body } = await updateUserController.execute(request);
-
-  return response.status(statusCode).send(body);
-});
-
-app.delete('/api/users/:userId', async (request, response) => {
-  const deleteUserController = makeDeleteUserController();
-
-  const { statusCode, body } = await deleteUserController.execute(request);
-
-  return response.status(statusCode).send(body);
-});
+app.use('/api/users', usersRouter);
 
 app.get('/api/transactions', async (request, response) => {
   const getTransactionsByUserIdController =
